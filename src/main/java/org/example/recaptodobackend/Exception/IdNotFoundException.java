850e21e711fd35d5712da3a833ac977134cf3e20
learnjavabh <learnmsbh@outlook.de>
2025-04-29 22:03:01 +0200

    added handling for IdNotFoundException

--- src/main/java/org/example/recaptodobackend/Exception/IdNotFoundException.java
+++ src/main/java/org/example/recaptodobackend/Exception/IdNotFoundException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class TodoNotFoundException extends RuntimeException {
-    public TodoNotFoundException(String id) {
+public class IdNotFoundException extends RuntimeException {
+    public IdNotFoundException(String id) {
         super("Todo with id "+id+" could not be found!");
     }
 }
